0010a299618648ffff28804971cd8a1f3d6ffbe6
sing27 <144003114+sing27@users.noreply.github.com>
2024-04-27 23:59:29 +0800

    Connect GCP DB

--- app/config.py
+++ app/config.py
@@ -2,11 +2,14 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+PASSWORD ="postgres"
+PUBLIC_IP_ADDRESS ="34.132.90.11:5432"
+DBNAME ="postgres"
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or \
-        'postgresql://postgres:postgres@postgresdb:5432/postgres'
+        f"postgresql://postgres:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or "mailhog"
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
